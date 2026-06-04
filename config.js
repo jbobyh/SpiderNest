4919bccf761e003ff9501a4255f5a1c7bc05ffb7
jbobyh <portalgup1@yandex.ru>
2026-06-04 15:32:09 +0500

    усилен 3 босс до 1000 хп

--- config.js
+++ config.js
@@ -512,7 +512,7 @@ const BOSS_DEFS = {
   },
   3: {
     type: 'boss_phase',
-    hp: 300,               // фиксированный HP
+    hp: 1000,               // фиксированный HP
     radiusMult: 2.5,
     speedMult: 1.0,
     name: 'БОСС',
